4ca5291c41e4edb4b3d2802559e0f30e41241fc9
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-12-14 17:58:06 -0500

    start btn and hit btn

---
+++ 21.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let html = document.querySelector("html");
+    let startGameBtn = document.createElement("button");
+    startGameBtn.type = "button";
+    startGameBtn.innerText = "Start Game";
+    
+    let startGameDiv = document.querySelector("#startGame");
+    startGameDiv.appendChild(startGameBtn);
+
+    let score = 0;
+    startGameBtn.addEventListener("click", async () => {
+        try {
+            let res1 = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            deckID = res1.data.deck_id;
+            // let cards = document.querySelector("#cards");
+            let res2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`);
+            startGameDiv.innerText = "";
+            for(let i = 0; i < res2.data.cards.length; i++) {
+                let img = document.createElement("img");
+                img.src = res2.data.cards[i].image;
+                startGameDiv.appendChild(img);
+                logScore(res2.data.cards[i].value);
+            } 
+            // scoreDiv.innerText = "Your score is: " + score;
+        }catch(err) {
+            debugger;
+        }
+    })
+
+    let buttonsDiv = document.querySelector("#buttons")
+    let hit = document.createElement("button");
+    hit.type = "button";
+    hit.innerText = "Hit";
+    buttonsDiv.appendChild(hit);
+
+    let stay = document.createElement("button");
+    stay.type = "button";
+    stay.innerText = "Stay";
+    buttonsDiv.appendChild(stay);
+
+    hit.addEventListener("click", async () => {
+        let newCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`);
+        let img = document.createElement("img");
+        img.src = newCard.data.cards[0].image;
+        // debugger;
+        startGameDiv.appendChild(img);
+        logScore(newCard.data.cards[0].value);
+    })
+
+    stay.addEventListener("click", () => {
+
+    })
+
+    const logScore = (value) => {
+        let scoreDiv = document.querySelector("#score");
+        if(value === "KING" || value === "QUEEN" || value === "JACK") {
+            score += 10;
+        } else if(value === "ACE" && score + 11 > 21) {
+            score += 1;
+        } else if(value === "ACE" && score + 11 < 21) {
+            score += 11;
+        } else {
+            score += Number(value);
+        }
+
+        if(Number(score) > 21) {
+            html.innerText = ""
+            let busted = document.createElement("h1");
+            busted.innerText = "Busted!";
+            html.appendChild(busted);
+        } else {
+            scoreDiv.innerText = "Your score is: " + score;
+        }
+        // debugger;
+    }
+})
